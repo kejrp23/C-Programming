044c6b32cb0c2b5c965ddac0b9a043a2c2c48dca
Kejrp23 <j.pittman51@att.net>
2024-03-08 23:33:50 -0500

    updated exercises

---
+++ Chapter 1/wordperline.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+
+int main(){
+
+		//write a program that takes input and prints one word per line.
+		char input[50];
+		scanf("%s",input);
+		printf("\n\n%s",input);
+
+
+		return 0;
+}
