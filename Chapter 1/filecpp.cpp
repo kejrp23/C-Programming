c17aeb10b40f4c70f658a4b386fe1be0ed823ed0
kejrp23 <j.pittman51@att.net>
2024-03-24 19:32:46 -0400

    updated file handling

--- Chapter 1/filecpp.cpp
+++ Chapter 1/filecpp.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include <fstream>
 
-
-
+struct employee{
+		char first[20];
+		char last[20];
+		int age;
+};
 
 int main(){
+		struct employee p1;
+		p1.first = "Jason";
+		
+		
+		
+		
 		std::fstream File{"sample1.txt"};
 
 				
--- Chapter 1/filework.c
+++ Chapter 1/filework.c
@@ -4,14 +4,23 @@
 
 const double pi = 3.14567;
 
+struct employee{
+		char first[20];
+		char last[20];
+		int age;
+};
 
 
 int main(){
+		struct employee p1;
+		sprintf(p1.first , "Jason");
+		sprintf(p1.last,"Pittman");
+		p1.age = 29;
 
 		FILE* fptr = fopen("sample.txt","w");
 		
-		char name[20] = "Jason";
-		fputs(name,fptr);
+		fputs(p1.first,fptr);
+		fputs(p1.last,fptr);
 		
 		
 		fclose(fptr);
