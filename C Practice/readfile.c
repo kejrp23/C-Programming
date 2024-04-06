03b1566b501587d43e545572cd862302c0472f89
Kejrp23 <j.pittman51@att.net>
2024-04-06 09:03:48 -0400

    updated my version of cat

--- C Practice/readfile.c
+++ C Practice/readfile.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-#define MAXLINES 1000
+char buffer[200];
 
-int main(){
+int main(int argc, char* argv[0]){
 
-		FILE* text;
-		text = fopen("test.txt","r");
-		char content[MAXLINES];
-		char c;
-		while ((c = fgetc(text)) != EOF){
+		FILE* fp;
+		fp = fopen(argv[1],"r");
+		if (fp != NULL){
+			char c;
+			while ((c = getc(fp)) != EOF){
 						printf("%c",c);
-						};
-
+				};
+		}
+		else{
+				printf("Error: Invalid Argument\n Example: ./runfile.exe filename.txt");
+		};
+								
 
 
 		return 0;
-}
-
-
-
+};
