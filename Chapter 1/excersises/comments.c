38f67fc78a75eedefaa3cab35dfd00d0fd5bc25a
Kejrp23 <j.pittman51@att.net>
2024-04-15 21:29:35 -0400

    excerisise 1.3 remove comments from file

---
+++ Chapter 1/excersises/comments.c
@@ -0,0 +1,26 @@
+//including headers
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+//including command line arguments
+int main(int arc, char* argv[]){
+
+		//declaring 3 int values;
+		int a=b=c=0;		
+
+		printf("Hello World");
+		
+		//this is a pointer
+		int* iptr = &a;
+
+		//print the pointer
+		printf("%d",*iptr);
+
+		return 0;
+
+};
+		
